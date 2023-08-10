27e8c3900a5a4c989f5e9112012a223e2e77fd55
msCrmDeveloper233 <141919535+msCrmDeveloper233@users.noreply.github.com>
2023-08-10 16:03:20 +0530

    Add files via upload

---
+++ Task1.cs
@@ -0,0 +1,85 @@
+﻿/*Task1: 
+ * Create a plugin that triggers on the "Create" event of a custom entity 
+ * and sets a default value for a specific attribute.
+ PPS=Post-Operation 
+ */
+
+//using System;
+//using Microsoft.Xrm.Sdk;
+
+
+//namespace PlugginPractice
+//{
+//   public class Task1 : IPlugin
+//    {
+//        public void Execute(IServiceProvider serviceProvider)
+//        {
+//            // throw new InvalidPluginExecutionException("this is generated from plugin");
+
+//            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+//            tracingService.Trace("1st");//debungin 
+//            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+           
+//            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+          
+//            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+            
+//            tracingService.Trace("2nd");
+
+//            Entity contact = new Entity("contact");
+//            contact["jobtitle"] = "sales manager";
+//            contact.Id = context.PrimaryEntityId;
+//            service.Update(contact);
+//            tracingService.Trace("3rd");
+          
+           
+//            //you can debug from plugin tracelog in crm and in vs studio as well by profiling 
+//        }
+//    }
+//}
+
+using System;
+using Microsoft.Xrm.Sdk;
+
+namespace PlugginPractice
+{
+     class Task1 : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            try
+            {
+                ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+                tracingService.Trace("1st"); // debugging
+
+                IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+                IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                tracingService.Trace("2nd");
+
+                // Call the custom method for the logic
+                PerformCustomLogic(service, context, tracingService);
+
+                tracingService.Trace("3rd");
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidPluginExecutionException("An error occurred: " + ex.Message);
+            }
+        }
+
+        // Custom method to encapsulate logic
+        private void PerformCustomLogic(IOrganizationService service, IPluginExecutionContext context, ITracingService tracingService)
+        {
+            Entity contact = new Entity("contact");
+            contact["jobtitle"] = "sales manager";
+            contact.Id = context.PrimaryEntityId;
+            service.Update(contact);
+
+            // Additional logic can be added here
+        }
+    }
+}
